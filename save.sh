4cf6f7bd1cef1c6a5614bc276d58c566b585e4c1
duan <naonao800@163.com>
2020-11-23 21:28:07 +0800

    添加wget依赖

--- save.sh
+++ save.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 rm -rfd n454149301-centos-openssh-server-centos8.tar
+rm -rfd n454149301-centos-openssh-server-centos8.tar.xz
 sudo docker save "n454149301/centos-openssh-server:centos8" -o n454149301-centos-openssh-centos8.tar
+sudo xz -v n454149301-centos-openssh-server-centos8.tar
