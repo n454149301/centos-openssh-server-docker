5bdafb5151cb7e229372a5adb936bb3bf0481807
duan <naonao800@163.com>
2020-11-24 13:47:42 +0800

    调整xmlstarlet的安装顺序，放到python后面

--- save.sh
+++ save.sh
@@ -2,5 +2,5 @@
 
 rm -rfd n454149301-centos-openssh-server-centos8.tar
 rm -rfd n454149301-centos-openssh-server-centos8.tar.xz
-sudo docker save "n454149301/centos-openssh-server:centos8" -o n454149301-centos-openssh-centos8.tar
+sudo docker save "n454149301/centos-openssh-server:centos8" -o n454149301-centos-openssh-server-centos8.tar
 sudo xz -v n454149301-centos-openssh-server-centos8.tar
